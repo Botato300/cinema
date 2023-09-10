86c06ce6321aac9f61b134326547899d487b9307
Votati <tomasbottari300@gmail.com>
2023-09-09 21:37:41 -0300

    .gitignore config

--- js/main.js
+++ js/main.js
@@ -9,11 +9,13 @@ const manager = new MovieManager();
 	const movies = manager.getMovies;
 	console.table(movies);
 
+	console.log("esto no debe salir");
+
 	const mainContainer = document.getElementById("main");
 	mainContainer.innerHTML = `
 		<section class="primary-movie">
-			<span class="primary-movie__title">${movies[1].getTitle}</span>
-			<img src="${movies[1].getCoverImage}" alt="Imagen de portada de la pelicula ${movies[1].getTitle}" class="primary-movie__image" width="auto" height="auto">
+			<span class="primary-movie__title">${movies[3].getTitle}</span>
+			<img src="${movies[3].getCoverImage}" alt="Imagen de portada de la película ${movies[3].getTitle}" class="primary-movie__image" width="auto" height="auto">
 		</section>
 	`;
 })()
